dc9658a59ab8f7f37d942d235e82aae8001286c1
Pingvu <nhlpool27@gmail.com>
2023-10-17 14:38:30 +0200

    Add project files.

---
+++ inlämning.3.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Upggift
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("har du slutat gymnasiet? svara med J eller N");
+            string gymnasie = Console.ReadLine().ToLower();
+
+            Console.WriteLine("hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (gymnasie == "j" && ålder < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
